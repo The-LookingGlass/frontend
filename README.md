8de089aaa204bc8467ba465b83fa3e5b9326ba1b
shivam sachdeva <techno848@gmail.com>
2018-06-24 22:57:29 +0100

    the working stuff

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# frontend
+This example [choropleth](http://bl.ocks.org/4060606) encodes road deaths in the Republic of Ireland in 2010. The data is taken from the Irish [Road Safety Authority](http://www.rsa.ie/en/RSA/Road-Safety/Our-Research/Deaths-injuries-on-Irish-roads/). Irish geoJSON data based on [https://gist.github.com/2183412](https://gist.github.com/2183412). Colours by [Colorbrewer](http://www.colorbrewer2.org).
---
+++ choropleth.js
@@ -0,0 +1,38 @@
+var data; // loaded asynchronously
+
+//Albers projection values based on playing with ireland.json using D3's Albers Example
+var proj = d3.geo.albers()
+          .origin([-7.9,53.3])
+          .scale(10000)
+          .translate([450,350]);
+
+var path = d3.geo.path().projection(proj);
+
+var svg = d3.select("#chart")
+  .append("svg");
+
+var counties = svg.append("g")
+    .attr("id", "ireland");
+
+//Irish geoJSON based on https://gist.github.com/2183412
+d3.json("ireland.json", function(json) {
+  counties.selectAll("path")
+      .data(json.features)
+    .enter().append("path")
+      .attr("class", "ireland")
+      .attr("d", path)
+      .on('mouseover', function(d){
+        var name = d.properties.STATE_ABBR;
+        return document.getElementById('name').innerHTML=name;
+    });
+});
+
+d3.json("road-deaths-2010.json", function(json) {
+  data = json;
+  counties.selectAll("path")
+      .attr("class", quantize);
+});
+ 
+function quantize(d) {
+  return "q" + Math.min(8, ~~(data[d.properties.id] * 9 / 21)) + "-9";
+}
