ce78878be2f4863dc3ab3a92243f61522093c51e
shivam sachdeva <techno848@gmail.com>
2018-07-18 22:55:26 +0100

    adding marker functionality

--- Website/googlemaps.js
+++ Website/googlemaps.js
@@ -1,38 +1,70 @@
 // google map api key
 var key = "AIzaSyAGiNsd-zPf5pwe6_JN32W5fth_yhLtTt0";
-// main function
-myMap();
-
-// loading county data json file
-// code used from https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
 
 function myMap(data) {
     // county location
     var location = getAllUrlParams().county;
 
+   
 
-// this requests the file and executes a callback with the parsed result once
-//   it is available
+    // this requests the file and executes a callback with the parsed result once
+    //   it is available
     readTextFile("county_data.json", function(text){
         var data = JSON.parse(text);
         
-        // fetching data from the json file
-       var long = data[location].longitude;
+        // fetching data from the json file for centering the map
+        var long = data[location].longitude;
         var lat =  data[location].latitude;
         var zoom = data[location].zoom;
 
-        
+        // properties for map 
         var mapProp= {
             center : new google.maps.LatLng(lat,long),
             zoom : zoom,
         };
         
+        // initializing the map
         var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    });
+
+
+         // ADDING MARKERS----
+
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        
+        // test locations 
+        // add cordinates to show markers on map
+        var locations = [
+          {lat:53.341676 , lng:-6.249326}
+        ];
+
+        // Marker properties
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+          position: location,
+          label: labels[i % labels.length]
+          });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+          {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+        });
 
 
 }
-    
+
+// using to place markers all over the map
+function placeMarkers(){
+
+  
+  
+}
+ 
+// loading county data json file
+// code used from https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+
+// reading the JSON file
 function readTextFile(file, callback) {
         var rawFile = new XMLHttpRequest();
         rawFile.overrideMimeType("application/json");
