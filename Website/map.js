c24dfd414714b0b7536110f5a93a366d3ac89659
shivam sachdeva <techno848@gmail.com>
2018-08-20 16:56:17 +0100

    updating latest code in minified files

--- Website/map.js
+++ Website/map.js
@@ -64,8 +64,10 @@ d3.json("ireland.json", function(json) {
 
 // loading data for the map to use
   d3.json("data/job_data_heatmap.json", function(json) {
+    
     // choose the industry specific data from json file
   data = json[industry];
+  
   counties.selectAll("path").attr("class", quantize);
 });
 
